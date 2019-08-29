d9b4c1afa341b41988e81b5d9317daa89ea6ae07
dolway <dolwayDu@isoftstone.com>
2019-08-29 15:05:53 +0800

    add 保存到相册1Demo

---
+++ 保存到相册1/保存到相册/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  保存到相册
+//
+//  Created by dolway on 2019/8/26.
+//  Copyright © 2019 iss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+    return 0;
+}
+
